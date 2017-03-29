a252faab5d74cfeb2a2ba9751a27c55f3dff85df
DeDM1sha <misha739@yandex.ru>
2017-03-30 01:03:27 +0300

    New version

--- main.cpp
+++ main.cpp
@@ -4,16 +4,13 @@
 #include <fstream>
 #include <time.h>
 #include <string.h>
-void ConfigureConsoleWindow ()
+void ConfigureConsoleWindow (bool Choise)
 {
     void* handle = GetStdHandle(STD_OUTPUT_HANDLE);
 	CONSOLE_CURSOR_INFO structCursorInfo;
 	GetConsoleCursorInfo(handle,&structCursorInfo);
-	structCursorInfo.bVisible = FALSE;
+	structCursorInfo.bVisible = Choise;
 	SetConsoleCursorInfo( handle, &structCursorInfo ); // Отключение каретки.
-	//setlocale (LC_ALL, "Russian"); // Русская кодировка текста.
-	system ("color B"); // Светло-голубой цвет текста.
-	system ("title chat"); // Заголовок программы.
 } // Функция для настройки консольного окна.
 
 void Center (std::string Stroke)
@@ -41,7 +38,7 @@ char Getch (std::string Menu)
 						std::cout.flush ();
 						exit (0);
 					}
-					if (Button == 49 || Button == 50)
+					if (Button == 49 || Button == 50 || Button == 51)
 						break;
 			}
 		}
@@ -49,23 +46,30 @@ char Getch (std::string Menu)
 			while (true)
 			{
 				Button = getch ();
-					if (Button == 50 || Button == 49 || Button == 27)
+					if (Button == 50 || Button == 49 || Button == 9)
 						break;
 			}
 		if (Menu == "Color_Settings")
 			while (true)
 			{
 				Button = getch ();
-					if ((Button >= 49 && Button <= 55) || (Button == 27))
+					if ((Button >= 49 && Button <= 55) || (Button == 9))
 						break;
 			}
 		if (Menu == "Background_Settings")
 			while (true)
 			{
 				Button = getch ();
-					if ((Button >= 48 && Button <= 55) || (Button == 27))
+					if ((Button >= 48 && Button <= 55) || (Button == 9))
 						break;
 			}
+		if (Menu == "Reference")
+			while (true)
+			{
+				Button = getch ();
+					if (Button == 9)
+						break;
+			}	
 	return Button;
 }
 void CoutCenterNameColor (std::string Stroke, unsigned short int ColorNumber)
@@ -79,7 +83,10 @@ int main ()
 {
 	unsigned short int Button = 0, ColorNumber = 11, BackgroundColorNumber = 0;
 	char NickName [12] = {' ',' ',' ',' ',' ',' ',' ',' ',' ', ' '}; // Этот массив нужен для того, чтобы не производить проверки на длину введенного никнейма, вся строка автоматически обрежется под эти 12 символов
-	ConfigureConsoleWindow ();
+	std::string Message;
+	system ("color B"); // Светло-голубой цвет текста.
+	system ("title chat_user_1"); // Заголовок программы.
+	ConfigureConsoleWindow (false);
 	while (true)
 	{
 		cls ();
@@ -91,12 +98,14 @@ int main ()
 		Center ("█────███────██───███████────██─██─██─██─███─██\n");
 		Center (" ██████████████████████████████████████████████\n\n");
 		Center ("__________________________________\n");
-		Center ("|            Меню чата           |\n");
+		Center ("|           Меню чата            |\n");
 		Center ("|                                |\n");
 		Center ("|--------------------------------|\n");
 		Center ("| 1. Войти в чат                 |\n");
 		Center ("|--------------------------------|\n");
 		Center ("| 2. Настройки                   |\n");
+		Center ("|--------------------------------|\n");
+		Center ("| 3. Справка                     |\n");
 		Center ("|--------------------------------|\n");
 		Center ("| Esc. Выйти из программы        |\n");
 		Center ("|________________________________|\n");
@@ -104,6 +113,7 @@ int main ()
 			if (Button == 49) // chat
 			{
 				cls ();
+				ConfigureConsoleWindow (true);
 				Center ("██████████████████████████████████████████████████████████████████\n");
 				Center ("█────██────███───██────██────██─██─██───██────██─██─███─██─██────█\n");
 				Center ("█─██─██─██──███─███─██─██─██─██─██─████─██─██─██─██─███─██─██─██─█\n");
@@ -113,96 +123,149 @@ int main ()
 				Center ("   ██████████████████████████████████████████████████████████████████\n\n\n\n");
 				Center ("Введите свой никнейм: ");
 				std::cin >> NickName;
+				/*std::ofstream Write ("E:\\Project\\UserMassagesOne.txt");
+				Write >> "";
+				Write.close ();*/ //очистка файла с сообщениями первого клиента
+				cls ();
+				Center ("Добро пожаловать в чат, ");
+					for (unsigned short int i = 0; i < 12; i++)
+						std::cout << NickName[i];
+				std::cout << "\n\n";
+					while (true)
+					{ 
+						getline(std::cin, Message);
+						std::cout << "\n";
+						std::cout << NickName << ": ";
+					}
+				std::cout << "\n";
+				system ("pause");
 			} // конец меню с чатом
 			if (Button == 50) // setings
 			{
-				cls ();
-				Center ("█████████████████████████████████████──██████████████\n");
-				Center ("█─██─██────██────██───██────██────██─██─██─██─██─██─█\n");
-				Center ("█─██─██─██─██─██─███─███─██─██─██─██─██─██─█─███─██─█\n");
-				Center ("█────██────██─██████─███────██─██─██─█──██──████─█──█\n");
-				Center ("█─██─██─██─██─██─███─███─█████─██─██──█─██─█─███──█─█\n");
-				Center ("█─██─██─██─██────███─███─█████────██─██─██─██─██─██─█\n");
-				Center (" █████████████████████████████████████████████████████\n\n");
-				Center ("__________________________________\n");
-				Center ("|            Меню настроек       |\n");
-				Center ("|                                |\n");
-				Center ("|--------------------------------|\n");
-				Center ("| 1. Цвет символов               |\n");
-				Center ("|--------------------------------|\n");
-				Center ("| 2. Цвет заднего фона           |\n");
-				Center ("|--------------------------------|\n");
-				Center ("| Esc. Вернуться в меню          |\n");
-				Center (" |________________________________|\n\n");
-				Button = Getch ("Settings");
-					if (Button == 49) // настройки цвета символов
-					{
-						SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), (WORD)((0 << 4) | 9));
-						Center ("1. Синий\n");
-						SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), (WORD)((0 << 4) | 10));
-						Center ("2. Зеленый\n");
-						SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), (WORD)((0 << 4) | 11));
-						Center ("3. Голубой\n");
-						SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), (WORD)((0 << 4) | 12));
-						Center ("4. Красный\n");
-						SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), (WORD)((0 << 4) | 13));
-						Center ("5. Лиловый\n");
-						SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), (WORD)((0 << 4) | 14));
-						Center ("6. Желтый\n");
-						SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), (WORD)((0 << 4) | 15));
-						Center ("7. Белый\n");
-						Button = Getch ("Color_Settings");
-							switch (Button)
-							{
-								case 49: ColorNumber = 9;
-								break;
-								case 50: ColorNumber = 10;
-								break;
-								case 51: ColorNumber = 11;
-								break;
-								case 52: ColorNumber = 12;
-								break;
-								case 53: ColorNumber = 13;
-								break;
-								case 54: ColorNumber = 14;
-								break;
-								case 55: ColorNumber = 15;
-								break;
-							}
-						Button = 0;
-					} // Конец настроек цвета символа
-					if (Button == 50) // настройки заднего фона
-					{
-						CoutCenterNameColor("0. Черный \n",11);
-						CoutCenterNameColor("1. Светло-синий\n",9);
-						CoutCenterNameColor("2. Светло-зеленый\n",10);
-						CoutCenterNameColor("3. Светло-голубой\n",11);
-						CoutCenterNameColor("4. Светло-красный\n",12);
-						CoutCenterNameColor("5. Светло-лиловый\n",13);
-						CoutCenterNameColor("6. Светло-желтый\n",14);	
-						CoutCenterNameColor("7. Ярко - белый\n",15);
-						Button = Getch ("Background_Settings");
-							switch (Button)
-							{
-								case 48: BackgroundColorNumber = 0;
-								break;
-								case 49: BackgroundColorNumber = 9;
-								break;
-								case 50: BackgroundColorNumber = 10;
-								break;
-								case 51: BackgroundColorNumber = 11;
-								break;
-								case 52: BackgroundColorNumber = 12;
-								break;
-								case 53: BackgroundColorNumber = 13;
-								break;
-								case 54: BackgroundColorNumber = 14;
-								break;
-								case 55: BackgroundColorNumber = 15;
-								break;
-							}
-					} // Конец настроек цвета заднего фона
-				SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), (WORD)((BackgroundColorNumber << 4) | ColorNumber));
+				while (true)
+				{
+					cls ();
+					Center ("█████████████████████████████████████──██████████████\n");
+					Center ("█─██─██────██────██───██────██────██─██─██─██─██─██─█\n");
+					Center ("█─██─██─██─██─██─███─███─██─██─██─██─██─██─█─███─██─█\n");
+					Center ("█────██────██─██████─███────██─██─██─█──██──████─█──█\n");
+					Center ("█─██─██─██─██─██─███─███─█████─██─██──█─██─█─███──█─█\n");
+					Center ("█─██─██─██─██────███─███─█████────██─██─██─██─██─██─█\n");
+					Center (" █████████████████████████████████████████████████████\n\n");
+					Center ("__________________________________\n");
+					Center ("|         Меню настроек          |\n");
+					Center ("|                                |\n");
+					Center ("|--------------------------------|\n");
+					Center ("| 1. Цвет символов               |\n");
+					Center ("|--------------------------------|\n");
+					Center ("| 2. Цвет заднего фона           |\n");
+					Center ("|--------------------------------|\n");
+					Center ("| Tab. Вернуться в меню          |\n");
+					Center (" |________________________________|\n\n");
+					Button = Getch ("Settings");
+						if (Button == 9)
+							break;
+						if (Button == 49) // настройки цвета символов
+						{
+							SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), (WORD)((BackgroundColorNumber << 4) | 9));
+							Center ("1. Синий\n");
+							SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), (WORD)((BackgroundColorNumber << 4) | 10));
+							Center ("2. Зеленый\n");
+							SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), (WORD)((BackgroundColorNumber << 4) | 11));
+							Center ("3. Голубой\n");
+							SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), (WORD)((BackgroundColorNumber << 4) | 12));
+							Center ("4. Красный\n");
+							SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), (WORD)((BackgroundColorNumber << 4) | 13));
+							Center ("5. Лиловый\n");
+							SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), (WORD)((BackgroundColorNumber << 4) | 14));
+							Center ("6. Желтый\n");
+							SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), (WORD)((BackgroundColorNumber << 4) | 15));
+							Center ("7. Белый\n");
+							Button = Getch ("Color_Settings");
+								switch (Button)
+								{
+									case 49: ColorNumber = 9;
+									break;
+									case 50: ColorNumber = 10;
+									break;
+									case 51: ColorNumber = 11;
+									break;
+									case 52: ColorNumber = 12;
+									break;
+									case 53: ColorNumber = 13;
+									break;
+									case 54: ColorNumber = 14;
+									break;
+									case 55: ColorNumber = 15;
+								}
+							Button = 0;
+						} // Конец настроек цвета символа
+						if (Button == 50) // настройки заднего фона
+						{
+							CoutCenterNameColor("0. Черный \n",0);
+							SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), (WORD)((BackgroundColorNumber << 4) | ColorNumber));
+							CoutCenterNameColor("1. Светло-синий\n",9);
+							SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), (WORD)((BackgroundColorNumber << 4) | ColorNumber));
+							CoutCenterNameColor("2. Светло-зеленый\n",10);
+							SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), (WORD)((BackgroundColorNumber << 4) | ColorNumber));
+							CoutCenterNameColor("3. Светло-голубой\n",11);
+							SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), (WORD)((BackgroundColorNumber << 4) | ColorNumber));
+							CoutCenterNameColor("4. Светло-красный\n",12);
+							SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), (WORD)((BackgroundColorNumber << 4) | ColorNumber));
+							CoutCenterNameColor("5. Светло-лиловый\n",13);
+							SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), (WORD)((BackgroundColorNumber << 4) | ColorNumber));
+							CoutCenterNameColor("6. Светло-желтый\n",14);	
+							SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), (WORD)((BackgroundColorNumber << 4) | ColorNumber));
+							CoutCenterNameColor("7. Ярко - белый\n",15);
+							Button = Getch ("Background_Settings");
+								switch (Button)
+								{
+									case 48: BackgroundColorNumber = 0;
+									break;
+									case 49: BackgroundColorNumber = 9;
+									break;
+									case 50: BackgroundColorNumber = 10;
+									break;
+									case 51: BackgroundColorNumber = 11;
+									break;
+									case 52: BackgroundColorNumber = 12;
+									break;
+									case 53: BackgroundColorNumber = 13;
+									break;
+									case 54: BackgroundColorNumber = 14;
+									break;
+									case 55: BackgroundColorNumber = 15;
+								}
+						} // Конец настроек цвета заднего фона
+					SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), (WORD)((BackgroundColorNumber << 4) | ColorNumber));
+				}
 			} // конец меню с настройками
+			
+			if (Button == 51) // setings
+			{
+				cls ();
+				Center ("███████████████████████████████████████████\n");
+				Center ("█────██────██────██────██────███─██─██────█\n");
+				Center ("█─██─██─██─██─██─██─██─██─██──██─█─███─██─█\n");
+				Center ("█─█████─██─██────██────██────███──████────█\n");
+				Center ("█─██─██─██─██─█████─██─██─██──██─█─███─██─█\n");
+				Center ("█────██─██─██─█████─██─██────███─██─██─██─█\n");
+				Center ("███████████████████████████████████████████\n\n");
+				Center ("__________________________________\n");
+				Center ("|                                |\n");
+				Center ("|      Версия программы: 1.0     |\n");
+				Center ("|--------------------------------|\n");
+				Center ("|        Разработчики :          |\n");
+				Center ("|                                |\n");
+				Center ("| Betterthan_Drugs               |\n");
+				Center ("| DeDxM1shaz                     |\n");
+				Center ("|--------------------------------|\n");
+				Center ("| Программа работает в           |\n");
+				Center ("| локальной сети(Wi-Fi)          |\n");
+				Center ("|--------------------------------|\n");
+				Center ("| Tab. Вернуться в меню          |\n");
+				Center (" |________________________________|\n\n");
+				Button = Getch ("Reference");
+		}
 	}
 }
